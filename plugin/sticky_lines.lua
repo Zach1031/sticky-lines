bd042db5dc86e737ed5cdd5880c01cd9d06b28fc
Zach1031 <zachdef31@gmail.com>
2023-12-07 16:41:36 -0500

    Removed unnecessary code

--- plugin/sticky_lines.lua
+++ plugin/sticky_lines.lua
@@ -7,22 +7,6 @@ function M.new()
 	vim.keymap.set("n", "<leader>k", ":lua require('sticky_lines').set()<CR>", { silent = true })
 	vim.keymap.set("n", "<leader>j", ":lua require('sticky_lines').jump()<CR>", { silent = true })
 end
-function M.set()
-	local curr_line = api.nvim_win_get_cursor(0)
-
-	last = { [0] = curr_line[0], [1] = curr_line[1] }
-
-	print(string.format("Setting sticky at (%d, %d)", curr_line[1], curr_line[2]))
-end
-
-function M.jump()
-	print(last)
-	local row, col = last[0], last[1]
-
-	print(string.format("Jumping to stick at (%d, %d)", row, col))
-
-	api.nvim_win_set_cursor(0, { row, col })
-end
 
 M.new()
 return M
